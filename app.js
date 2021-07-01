b1bb795006ab51a6b41b137d24945066d719695a
49985 <amoiz222862@gmail.com>
2021-07-01 21:58:04 +0500

    objects-js

---
+++ app.js
@@ -0,0 +1,81 @@
+// ********************** OBJECTS **************************
+
+
+//      TASK 1   
+
+
+//     {name:"juice",price:50, quantity:3,
+//     caculate : function(){
+//         return this.price * this.quantity;}},
+
+//     {name:"cookie",price:30, quantity:9,
+//     caculate : function(){
+//         return this.price * this.quantity;}},
+
+//     {name:"shirt",price:880, quantity:1,
+//     caculate : function(){
+//         return this.price * this.quantity;}},
+
+//     {name:"pen",price:100, quantity:2,
+//     caculate : function(){
+//         return this.price * this.quantity;}},]
+
+//     console.log(itemsArray[0].caculate());
+//     console.log(itemsArray[1].caculate());
+//     console.log(itemsArray[2].caculate());
+//     console.log(itemsArray[3].caculate());
+
+
+//    TASK 2
+
+
+// var maleData = {
+
+//     firstName: "abdul moiz",
+//     lastName: "zahid",
+//     email: "abdulmoiz@gmail.com",
+//     phone: "038087462093",
+//     age: 21,
+
+
+//     getFullName: function () {
+// return this.firstName + " " + this.lastName
+//     }
+
+
+// }
+
+// console.log(maleData.getFullName())
+// console.log(maleData)
+
+
+//    TASK 3
+
+
+
+// function Studentsdata(name, email, gender, phone, department, course) {
+//     this.name = name;
+//     this.gender = gender;
+//     this.email = email;
+//     this.phone = phone;
+//     this.department = department;
+//     this.course = course;
+//     this.timings = {
+//         day : "monday",
+//         time: "10am"
+//     }
+
+// }
+
+
+
+// let Studentsdata =[
+//     new student = ("abdul moiz","male","amoiz@gmail.com")
+// new student = ("abdul rehman","male","a_rehman@gmail.com")
+// ]
+
+
+
+
+
+
